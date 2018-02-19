de286a198a950b77156a044be130195b76ed176e
nilpatil340 <nilpatil340@gmail.com>
2018-02-19 16:59:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,22 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-var img = document.getElementById('madi');
-img.onclick = function(){
-    img.style.marginLeft = '100px';
+var button = document.getElementById("counter");
+
+button.onclick = function(){
+  
+  var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function() {
+      
+      if(request.readyState === XMLHttpRequest.Done){
+          
+          if(request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
+  
+  request.open('GET','http://nilpatil340.imad.hasura-app.io/counter',true);
+  request,send(null);
+    
 };

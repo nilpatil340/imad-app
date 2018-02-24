8bfafac402f20da4f2e8633cf1a1d0dce32330d7
nilpatil340 <nilpatil340@gmail.com>
2018-02-24 13:29:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = required('pg').pool;
+
+var config = {
+    user : 'nilpatil340',
+    database : 'nilpatil340',
+    port : '5555',
+    host : 'db.imad.hasura-app.io',
+    password : 'process.env.db_password'
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -82,6 +91,18 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res) {
+    
+    pool.query('SELECT * from test',function(err,res){
+       if(err){
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 var counter=0;
